d4d8b44db468d2986ffa4d71e184f0fc18b8d802
ritikrajput019 <108268201+ritikrajput019@users.noreply.github.com>
2023-08-23 17:22:47 +0530

    Update database.php

--- database.php
+++ database.php
@@ -1,7 +1,7 @@
 <?php
 $servername="localhost";
-$username="id20821197_restauantwebsite";
-$password="12345@Acro123";
+$username="root";
+$password="";
 $con= mysqli_connect($servername,$username,$password,$dbname);
 if(!$con){
 
