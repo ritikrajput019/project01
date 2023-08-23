313a53a9c601c8b48f79a6fdf184e0c1316e9503
ritikrajput019 <108268201+ritikrajput019@users.noreply.github.com>
2023-08-23 17:22:18 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-	$conn = new mysqli("localhost","restauantwebsite","","restaurant");
+	$conn = new mysqli("localhost","root","","restaurant");
 	if($conn->connect_error){
 		die("Connection Failed!".$conn->connect_error);
 	}
